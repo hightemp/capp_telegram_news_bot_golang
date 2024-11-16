04be451100664a690d8d92e31903ead2a8421290
hightemp <hightemp.unknown@gmail.com>
2024-11-16 13:49:34 +0300

    update

--- release.sh
+++ release.sh
@@ -5,4 +5,4 @@ CFILE="capp_telegram_news_bot_golang"
 timestamp=$(date +%s)
 VERSION=$(echo `cat VERSION`.$timestamp)
 
-gh release create $VERSION -t $VERSION -n "" $CFILE
+gh release create $VERSION -t $VERSION -n "" $CFILE ${CFILE}_static
